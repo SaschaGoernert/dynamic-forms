757bb95f1427c556eb57d0cb266d2db756b71e45
Alexander Gebuhr <AlexanderGebuhr@outlook.com>
2019-06-13 07:48:13 +0200

    Added material panel.

--- libs/material/src/lib/dynamic-form-wrapper/dynamic-form-panel/dynamic-form-panel.module.ts
+++ libs/material/src/lib/dynamic-form-wrapper/dynamic-form-panel/dynamic-form-panel.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatDynamicFormPanelComponent } from './dynamic-form-panel.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatExpansionModule
   ],
   declarations: [
     MatDynamicFormPanelComponent
